ee65f2911bba92a947816562081a174cf6a518fc
Gishaan <ishaangupta@Ishaans-MacBook-Pro.local>
2017-02-17 19:00:52 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$url = parse_url(getenv("heroku_8c0cd6c1d489728	"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
